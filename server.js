428541ede786621e02db9cefa53e58944f0c1fc8
Chansu Jo <whckstn12@naver.com>
2021-01-20 15:31:45 +0900

    redis 받아와서 클라이언트를 생성 후 페이지에 접속할 때마다 숫자를 1씩 올리도록 설정

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const redis = require('redis');
 
 const PORT = 8080;
 
+//redis 클라이언트 생성
+const client = redis.createClient({
+    host: "redis-server",
+    port: 6379
+});
+
 const app = express();
+
+//숫자는 0부터 시작
+client.set("number", 0);
+
 app.get('/', (req,res) => {
-    res.send("Hello World")
+    client.get("number", (err, number) => {
+        client.set("number", parseInt(number) + 1)
+        res.send("숫자가 1씩 올라갑니다. 숫자: " + number)
+    })
 });
 
 app.listen(PORT)
